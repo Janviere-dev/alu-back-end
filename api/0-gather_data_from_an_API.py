ff1e1e06605a6c9f0f802d5144721794d8237ebe
Janviere-dev <j.munezero@alustudent.com>
2025-07-07 14:21:31 -0700

    gathering data from api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,8 +7,12 @@ if __name__ == "__main__":
     employee_id = sys.argv[1]
 
     # Base URLs
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id
+    )
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        employee_id
+    )
 
     # Fetch user info
     user_response = requests.get(user_url)
@@ -25,7 +29,10 @@ if __name__ == "__main__":
     done_tasks = len(completed_tasks)
 
     # Display progress
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, done_tasks, total_tasks))
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, done_tasks, total_tasks
+    ))
     for task in completed_tasks:
         print("\t {}".format(task.get("title")))
+
 
